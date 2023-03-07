005a18afd9276dc666cf50d1b414317541f2f14b
Hest0 <hesto365@gmail.com>
2023-03-07 09:52:54 +0200

    added needed modules and the url to scrape

---
+++ nhl_scraper.py
@@ -0,0 +1,7 @@
+import csv
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+#first the url to scrape
+url = 'https://www.quanthockey.com/nhl/nationality/finnish-nhl-players-2022-23-stats.html'
