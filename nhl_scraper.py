85ff8a5b8b562073a511058e4ac0246a66006933
Hest0 <hesto365@gmail.com>
2023-03-07 10:08:39 +0200

    Created objects 'response, html, soup, now, date_str, filename, rows and stats'

--- nhl_scraper.py
+++ nhl_scraper.py
@@ -5,3 +5,21 @@ from bs4 import BeautifulSoup
 
 #first the url to scrape
 url = 'https://www.quanthockey.com/nhl/nationality/finnish-nhl-players-2022-23-stats.html'
+
+#send a request to the URL to get the HTML content
+response = requests.get(url)
+html = response.content
+
+#create an object to parse HTML
+soup = BeautifulSoup(html, 'html.parser')
+
+#get the date of the stats from the page title
+now = datetime.datetime.now()
+date_str = now.strftime("%Y-%m-%d")
+filename = f"nhl_stats_{date_str}.csv"
+
+#search for players with points last night
+rows = soup.select('tr.fin td:nth-of-type(10):not(:-soup-contains("0"))')
+
+#create a list to store the stats
+stats = []
