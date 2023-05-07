5733446437b8976b41705a9491a9a870c6cc345f
Hest0 <hesto365@gmail.com>
2023-05-08 00:32:11 +0300

    Saving the data to an sql file instead of Pages

--- nhl_scraper.py
+++ nhl_scraper.py
@@ -1,20 +1,35 @@
+import os
 import csv
 import datetime
 import requests
 from bs4 import BeautifulSoup
+import mysql.connector
 
-url = 'https://www.quanthockey.com/nhl/nationality/finnish-nhl-players-2022-23-stats.html'
+user = os.environ.get('MYSQL_USER')
+password = os.environ.get('MYSQL_PASSWORD')
+host = os.environ.get('MYSQL_HOST')
+database = os.environ.get('MYSQL_DATABASE')
+
+url = 'https://www.quanthockey.com/nhl/nationality/finnish-nhl-players-2022-23-playoff-stats.html'
 
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html, 'html.parser')
 
+cnx = mysql.connector.connect(
+    host=os.environ.get('MYSQL_HOST'),
+    user=os.environ.get('MYSQL_USER'),
+    password=os.environ.get('MYSQL_PASSWORD'),
+    database=os.environ.get('MYSQL_DATABASE')
+)
+
+cursor = cnx.cursor()
+
 now = datetime.datetime.now()
 date_str = now.strftime("%Y-%m-%d")
-filename = f"nhl_stats_{date_str}.csv"
 
-rows = soup.select('tr.fin td:nth-of-type(10):not(:-soup-contains("0"))')
+rows = soup.select('tr.reg:not(.tHead)')
 
 stats = []
 
@@ -22,15 +37,20 @@ for row in rows:
     
     name = row.select_one('td.player-name > a').text
 
-    goals = row.select_one('td:nth-of-type(5)').text
-    assists = row.select_one('td:nth-of-type(6)').text
+    goals = int(row.select_one('td:nth-of-type(5) > a')).text
+    assists = int(row.select_one('td:nth-of-type(6) > a')).text
 
     stats.append([date_str, name, goals, assists])
 
-with open(filename, 'a', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['date', 'name', 'goals', 'assists'])
-    writer.writerows(stats)
+insert_query = """"
+INSERT INTO fin_playoff_scores_2023 (date, name, goals, assists)
+VALUES (%s, %s, %s, %s)
+"""
+cursor.executemany(insert_query, stats)
+cnx.commit()
+
+cursor.close()
+cnx.close()
 
 import subprocess
-subprocess.call(['open', '-a', 'Numbers', filename])
+subprocess.call(['open', '-a', 'MySQLWorkbench', '/Users/heikki/projects/Python/nhl_scores/nhl_score_scraper/nhl_stats.sql'])
